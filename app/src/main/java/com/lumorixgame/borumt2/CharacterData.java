7783fd5e4b9adad61b3cd4fac1996933be42ec33
BORUMT2 <macutorun@gmail.com>
2026-09-12 15:54:30 +0000

    Add class-specific starting equipment

--- app/src/main/java/com/lumorixgame/borumt2/CharacterData.java
+++ app/src/main/java/com/lumorixgame/borumt2/CharacterData.java
@@ -27,8 +27,39 @@ public class CharacterData {
     }
 
     public void createStartingItems() {
-        Item weapon = new Item("Başlangıç Silahı", 10, 9, 0);
-        weapon.setFixedBonuses("Güç +10", "Saldırı Hızı +5%");
+        String weaponName;
+        String bonus1;
+        String bonus2;
+
+        switch (characterClass) {
+            case SAVASCI:
+                weaponName = "Başlangıç Kılıcı";
+                bonus1 = "Güç +10";
+                bonus2 = "Saldırı Hızı +5%";
+                break;
+            case NINJA:
+                weaponName = "Başlangıç Bıçağı";
+                bonus1 = "Çeviklik +10";
+                bonus2 = "Saldırı Hızı +5%";
+                break;
+            case SURA:
+                weaponName = "Başlangıç Kılıcı";
+                bonus1 = "Zeka +10";
+                bonus2 = "Büyülü Saldırı +5%";
+                break;
+            case SAMAN:
+                weaponName = "Başlangıç Yelpazesi";
+                bonus1 = "Zeka +10";
+                bonus2 = "Büyülü Saldırı +5%";
+                break;
+            default:
+                weaponName = "Başlangıç Silahı";
+                bonus1 = "Güç +10";
+                bonus2 = "Saldırı Hızı +5%";
+        }
+
+        Item weapon = new Item(weaponName, 10, 9, 0);
+        weapon.setFixedBonuses(bonus1, bonus2);
 
         Item armor = new Item("Başlangıç Zırhı", 10, 9, 0);
         armor.setFixedBonuses("Savunma +10", "HP +100");
